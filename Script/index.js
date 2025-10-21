6e73acad98cd7d3753a19dd89c5bfb22ae6909a6
AmusaAbdul <168703589+AmusaAbdul@users.noreply.github.com>
2025-10-21 19:22:35 +0100

    Add files via upload

--- Script/index.js
+++ Script/index.js
@@ -1,6 +1,19 @@
 function updateTime() {
   const timeElement = document.querySelector('[data-testid="test-user-time"]');
+  const now = new Date();
+  const formattedTime = now.toLocaleTimeString(); 
   timeElement.textContent = new Date().getTime();
 }
+
 setInterval(updateTime, 1000);
 updateTime();
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('[data-testid="test-user-social-twitter"]').onclick = function() {
+    window.open('https://x.com/brunolinlcoln?s=21', '_blank', 'noopener,noreferrer');
+  };
+
+  document.querySelector('[data-testid="test-user-social-github"]').onclick = function() {
+    window.open('https://github.com/AmusaAbdul', '_blank', 'noopener,noreferrer');
+  };
+});
---
+++ Script/pages.js
@@ -0,0 +1,26 @@
+const form = document.getElementById('contact-form');
+const successMessage = document.getElementById('test-contact-success');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const fullName = document.getElementById('full-name').value;
+  const email = document.getElementById('email').value;
+  const subject = document.getElementById('subject').value;
+  const message = document.getElementById('message').value;
+  if (fullName && email && subject && message.length >= 10) {
+    successMessage.style.display = 'block';
+  } else {
+    // Display error messages
+    if (!fullName) {
+      document.getElementById('test-contact-error-name').textContent = 'Full name is required';
+    }
+    if (!email) {
+      document.getElementById('test-contact-error-email').textContent = 'Email is required';
+    }
+    if (!subject) {
+      document.getElementById('test-contact-error-subject').textContent = 'Subject is required';
+    }
+    if (message.length < 10) {
+      document.getElementById('test-contact-error-message').textContent = 'Message must be at least 10 characters';
+    }
+  }
+});
